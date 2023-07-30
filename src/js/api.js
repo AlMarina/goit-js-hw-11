a55d5d45f715cd744565ac58ea9ad68ec722f06d
AlMarina <almarina1017@gmail.com>
2023-07-30 03:17:00 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -5,13 +5,10 @@ const API_KEY = '38535575-2b05e44024ae670a115fb0cfb';
 
 
 export default async function searchImages(name, page) {
-  try {
     const response = await axios.get(
       `${BASE_URL}?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`
     );
     return response.data;
-  } catch (error) {
-    console.error(error);
-  }
+ 
 }
 
